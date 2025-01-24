fd88b0478fa3251268c394c26d9624a39c252c33
Ravindu Pasanjith <IT22898920@my.sliit.lk>
2025-01-24 20:44:32 +0530

    part

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ app.all("*", (req, res) => {
 // Error Middleware
 app.use(errorHandler);
 // Start the server
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 7001;
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
